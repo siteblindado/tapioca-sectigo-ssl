f16e5b45bb607be9ce71c06b1f961bcbeac8f754
Pedro <pedro.hls427@gmail.com>
2019-08-12 18:25:27 -0300

    connecting with sectigo

--- tapioca_sectigo_api_ssl/__init__.py
+++ tapioca_sectigo_api_ssl/__init__.py
@@ -2,7 +2,7 @@
 
 __author__ = 'Site Blindado S.A.'
 __email__ = 'dev@siteblindado.com.br'
-__version__ = '0.2'
+__version__ = '0.3'
 
 
 from .tapioca_sectigo_api_ssl import Sectigo_api_ssl  # noqa
